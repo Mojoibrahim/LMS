5f1b24ad75955050ab70248997852c91a45d914c
mojoibrahim <mojoibrahim249@gmail.com>
2026-09-21 12:08:12 +0300

    updated files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Auth/Login';
 import Register from './Auth/Register';
-<<<<<<< HEAD
 import OtpVerification from './Auth/OtpVerification'; // Imported the new OTP component
-=======
->>>>>>> da284eba16984e2070799f996c7cc2fcab17cb4e
 import TestDashboard from './pages/TestDashboard';
 import './App.css'; // You can keep your existing CSS file
 
@@ -18,10 +15,7 @@ function App() {
         {/* Auth Routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-<<<<<<< HEAD
         <Route path="/otp-verification" element={<OtpVerification />} /> {/* New OTP Verification route added */}
-=======
->>>>>>> da284eba16984e2070799f996c7cc2fcab17cb4e
         
         {/* Protected/Test Page Route */}
         <Route path="/dashboard" element={<TestDashboard />} />
